77ba0ebfbd9171537c58a4940641fe0836a5c27d
absyalsatria20 <absyalsatria20@gmail.com>
2026-07-15 16:16:32 +0000

    Persiapan final untuk Vercel

--- api/index.php
+++ api/index.php
@@ -1,26 +1,3 @@
 <?php
-
-// 1. Paksa format JSON
-$_SERVER['HTTP_ACCEPT'] = 'application/json';
-
-// 2. Buat folder sementara khusus Vercel di memori /tmp
-$tmpStorage = '/tmp/storage';
-$directories = [
-    'framework/views',
-    'framework/cache',
-    'framework/cache/data',
-    'framework/sessions',
-    'logs'
-];
-
-foreach ($directories as $dir) {
-    if (!is_dir("$tmpStorage/$dir")) {
-        mkdir("$tmpStorage/$dir", 0777, true);
-    }
-}
-
-// 3. Beritahu Laravel untuk membuang file view ke /tmp
-putenv("VIEW_COMPILED_PATH=$tmpStorage/framework/views");
-$_ENV['VIEW_COMPILED_PATH'] = "$tmpStorage/framework/views";
-
+// Teruskan semua permintaan Vercel ke sistem utama Laravel
 require __DIR__ . '/../public/index.php';
