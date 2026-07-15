91a8c69ca008a7318459e216edbbca5d07bdc72a
absyalsatria20 <absyalsatria20@gmail.com>
2026-07-15 16:13:00 +0700

    Update api/index.php dengan folder tmp Vercel

--- api/index.php
+++ api/index.php
@@ -1,6 +1,26 @@
 <?php
 
-// Paksa Laravel untuk selalu membalas dengan format teks JSON (bukan HTML)
+// 1. Paksa format JSON
 $_SERVER['HTTP_ACCEPT'] = 'application/json';
+
+// 2. Buat folder sementara khusus Vercel di memori /tmp
+$tmpStorage = '/tmp/storage';
+$directories = [
+    'framework/views',
+    'framework/cache',
+    'framework/cache/data',
+    'framework/sessions',
+    'logs'
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir("$tmpStorage/$dir")) {
+        mkdir("$tmpStorage/$dir", 0777, true);
+    }
+}
+
+// 3. Beritahu Laravel untuk membuang file view ke /tmp
+putenv("VIEW_COMPILED_PATH=$tmpStorage/framework/views");
+$_ENV['VIEW_COMPILED_PATH'] = "$tmpStorage/framework/views";
 
 require __DIR__ . '/../public/index.php';
